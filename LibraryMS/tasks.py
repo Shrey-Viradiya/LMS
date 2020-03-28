bebd52525cba95cd23cef65d2b214d718f5e50d2
Shrey Viradiya <shreyviradiya@gmail.com>
2020-03-28 23:47:40 +0530

    change in Background Process

--- LibraryMS/tasks.py
+++ LibraryMS/tasks.py
@@ -13,40 +13,43 @@ def SendEmails():
 
     last_msg = f'End of the function {datetime.now()}\n'
 
-    hlds = BookHold.objects.filter(available=3)
-    # last_msg+=str(hlds)
-    msgs = (('Advanced Book Due Notice',
-             f'Dear {_.holder.first_name} {_.holder.last_name},\nYour book {_.book.title} is not picked up by you. Your hold is released',
-             'LibraryManagementSystem', [_.holder.email]) for _ in hlds)
-    send_mass_mail(msgs, fail_silently=True)
+    try:
+        hlds = BookHold.objects.filter(available=3)
+        # last_msg+=str(hlds)
+        msgs = (('Advanced Book Due Notice',
+                 f'Dear {_.holder.first_name} {_.holder.last_name},\nYour book {_.book.title} is not picked up by you. Your hold is released',
+                 'LibraryManagementSystem', [_.holder.email]) for _ in hlds)
+        send_mass_mail(msgs, fail_silently=True)
 
-    for _ in hlds:
-        _.book.availability+=1
-        _.book.save()
-        _.delete()
+        for _ in hlds:
+            _.book.availability+=1
+            _.book.save()
+            _.delete()
 
-    hlds = BookHold.objects.filter(available=1)
-    # last_msg += str(hlds)
-    for _ in hlds:
-        _.available+=1
-        _.save()
+        hlds = BookHold.objects.filter(available=1)
+        # last_msg += str(hlds)
+        for _ in hlds:
+            _.available+=1
+            _.save()
 
-    hlds = BookHold.objects.filter(available=2)
-    # last_msg += str(hlds)
-    for _ in hlds:
-        _.available += 1
-        _.save()
+        hlds = BookHold.objects.filter(available=2)
+        # last_msg += str(hlds)
+        for _ in hlds:
+            _.available += 1
+            _.save()
 
-    brd = BookBorrowed.objects.filter(due_date__day=today - timedelta(days=2))
-    # last_msg += str(brd)
-    msgs = (('Advanced Book Due Notice',f'Dear {book.borrower.first_name} {book.borrower.last_name},\nYour book {book.book.ISBN.title} will be due in 2 days','LibraryManagementSystem',[book.borrower.email]) for book in brd)
-    send_mass_mail(msgs,fail_silently=True)
+        brd = BookBorrowed.objects.filter(due_date__date=today - timedelta(days=2))
+        # last_msg += str(brd)
+        msgs = (('Advanced Book Due Notice',f'Dear {book.borrower.first_name} {book.borrower.last_name},\nYour book {book.book.ISBN.title} will be due in 2 days','LibraryManagementSystem',[book.borrower.email]) for book in brd)
+        send_mass_mail(msgs,fail_silently=True)
 
-    brd = BookBorrowed.objects.filter(due_date__day=today)
-    # last_msg += str(brd)
-    msgs = (('Book Due Notice',
-                 f'Dear {book.borrower.first_name} {book.borrower.last_name},\nYour book {book.book.ISBN.title} is due today. Please Return ASAP.',
-                 'LibraryManagementSystem', [book.borrower.email]) for book in brd)
-    send_mass_mail(msgs, fail_silently=True)
+        brd = BookBorrowed.objects.filter(due_date__date=today)
+        # last_msg += str(brd)
+        msgs = (('Book Due Notice',
+                     f'Dear {book.borrower.first_name} {book.borrower.last_name},\nYour book {book.book.ISBN.title} is due today. Please Return ASAP.',
+                     'LibraryManagementSystem', [book.borrower.email]) for book in brd)
+        send_mass_mail(msgs, fail_silently=True)
+    except :
+        admin.email_user('Testing Background Function', 'Some Error Occured')
 
     admin.email_user('Testing Background Function', last_msg)
